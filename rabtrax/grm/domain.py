81159963ba7f8b266089615aa94203843f37cfb1
Steven McCauley <steven_mccauley@brown.edu>
2019-08-13 14:05:34 -0400

    Namespaces

--- rabtrax/grm/domain.py
+++ rabtrax/grm/domain.py
@@ -2,13 +2,12 @@
 
 class Model(type):
     def __init__(cls, classname, bases, dct_):
-        type.__init__(cls, classname, bases, dct_)
+        super().__init__(classname, bases, dct_)
 
 
-def domain():
-    resource_attribute_map = { 'rdf_type': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' }
-    return Model('Model', (object,) , { 'lookup': resource_attribute_map })
-
+def domain(ontologies):
+    ns_map = { o.name: o for o in ontologies }
+    return Model('Resource', (object,) , { 'ontologies': ns_map })
 
 
 
@@ -20,10 +19,3 @@ class Domain:
 
     def setStore(self, store):
         self.store = store
-
-
-class RDFStatement:
-    def __init__(self, sbj=None, prop=None, obj=None):
-        self.sbj = sbj
-
-
---
+++ rabtrax/grm/ns.py
@@ -0,0 +1,48 @@
+class Ontology(object):
+
+    def __init__(self, namespace, prefix):
+        self.prefix = prefix
+        self.name = prefix
+        self.ns = namespace
+        self.properties = {}
+        self.classes = {}
+
+
+RDF = Ontology('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf')
+RDF.properties = {
+    'type': RDF.ns + 'type'
+}
+
+RDFS = Ontology('http://www.w3.org/2000/01/rdf-schema#', 'rdfs')
+RDFS.properties = {
+    'label': RDFS.ns + 'label'
+}
+
+VIVO = Ontology('http://vivoweb.org/ontology/core#', 'vivo')
+VIVO.properties = {
+    'overview': VIVO.ns + 'overview',
+    'research_overview': VIVO.ns + 'researchOverview',
+    'teaching_overview': VIVO.ns + 'teachingOverview',
+    'has_collaborator': VIVO.ns + 'hasCollaborator',
+    'has_research_area': VIVO.ns + 'hasResearchArea',
+}
+
+BPROFILE = Ontology('http://vivo.brown.edu/ontology/profile#', 'bprofile')
+BPROFILE.properties = {
+    'consents_visualizations': BPROFILE.ns + 'consentsVisualizations',
+    'has_appointment': BPROFILE.ns + 'hasAppointment',
+    'has_credential': BPROFILE.ns + 'hasCredential',
+    'has_training': BPROFILE.ns + 'hasTraining',
+}
+
+BLOCAL = Ontology('http://vivo.brown.edu/ontology/vivo-brown/', 'blocal')
+BLOCAL.properties = {
+    'affiliations': BLOCAL.ns + 'affiliations',
+    'awards_and_honors': BLOCAL.ns + 'awardsAndHonors',
+    'scholarly_work': BLOCAL.ns + 'scholarlyWork',
+    'funded_research': BLOCAL.ns + 'fundedResearch',
+    'drrb_web_page': BLOCAL.ns + 'drrbWebPage',
+    'research_statement': BLOCAL.ns + 'researchStatement',
+    'has_delegate': BLOCAL.ns + 'hasDelegate',
+    'has_geographic_research_area': BLOCAL.ns + 'hasGeographicResearchArea'
+}
